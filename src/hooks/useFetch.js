942295effc8195a2624f0ec150422bd979ed14b2
adramelech <adramelech.psd@gmail.com>
2021-12-09 19:11:07 +0530

    manually redirect to notfound page if error code is 404

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,20 +1,23 @@
 import { useCallback } from "react";
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { hideModal, showModal } from "../features/modalSlice";
 
 const useFetch = () => {
    const dispatch = useDispatch();
+   const navigate = useNavigate();
    return useCallback(
       async (callback, ...props) => {
          try {
             return await callback(...props);
          } catch (error) {
+            if (error.response.status === 404) navigate("/not-found");
             const { msg } = error.response?.data || "Something went wrong";
             dispatch(showModal(msg));
             setTimeout(() => dispatch(hideModal()), 3000);
          }
       },
-      [dispatch]
+      [dispatch, navigate]
    );
 };
 
