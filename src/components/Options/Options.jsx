ec6ca1920e89ad7472c94bba88485cb21a9bb3ed
adramelech <adramelech.psd@gmail.com>
2021-12-18 14:03:24 +0530

    none

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -3,22 +3,22 @@ import { optionsIcon } from "../../assets";
 import "./options.css";
 
 const Options = ({ deleteHandler }) => {
-   const [isOptionsVisible, setIsOptionsVisible] = useState(false);
-   return (
-      <div className="options">
-         <img src={optionsIcon} alt="" onClick={() => setIsOptionsVisible(val => !val)} />
-         <ul className={isOptionsVisible ? "show" : ""}>
-            <li
-               onClick={() => {
-                  setIsOptionsVisible(false);
-                  deleteHandler();
-               }}
-            >
-               Delete
-            </li>
-         </ul>
-      </div>
-   );
+    const [isOptionsVisible, setIsOptionsVisible] = useState(false);
+    return (
+        <div className="options" onClick={() => setIsOptionsVisible(val => !val)}>
+            <img src={optionsIcon} alt="options" />
+            <ul className={isOptionsVisible ? "show" : ""}>
+                <li
+                    onClick={() => {
+                        setIsOptionsVisible(false);
+                        deleteHandler();
+                    }}
+                >
+                    Delete
+                </li>
+            </ul>
+        </div>
+    );
 };
 
 export default Options;
