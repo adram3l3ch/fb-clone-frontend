81e50e227583de5b9a8ead5fcca2ad489f118452
adramelech <adramelech.psd@gmail.com>
2022-04-03 20:43:45 +0530

    add home nav icon

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,19 +1,20 @@
-export { default as cakeIcon } from "./cake.svg";
-export { default as cameraIcon } from "./camera.svg";
-export { default as clockIcon } from "./clock.svg";
-export { default as likeOutlined } from "./like-outlined.svg";
-export { default as likeIcon } from "./like.svg";
-export { default as locationIcon } from "./location.svg";
-export { default as mailIcon } from "./mail.svg";
-export { default as notFound } from "./not-found.svg";
-export { default as sendIcon } from "./plane.svg";
-export { default as searchIcon } from "./search.svg";
+export { default as cakeIcon } from './cake.svg';
+export { default as cameraIcon } from './camera.svg';
+export { default as clockIcon } from './clock.svg';
+export { default as likeOutlined } from './like-outlined.svg';
+export { default as likeIcon } from './like.svg';
+export { default as locationIcon } from './location.svg';
+export { default as mailIcon } from './mail.svg';
+export { default as notFound } from './not-found.svg';
+export { default as sendIcon } from './plane.svg';
+export { default as searchIcon } from './search.svg';
 
-export { default as chatIcon } from "./chat.svg";
-export { default as closeIcon } from "./close.svg";
-export { default as fileIcon } from "./file.svg";
-export { default as hamburger } from "./hamburger.svg";
-export { default as optionsIcon } from "./options.svg";
-export { default as logoutIcon } from "./logout.svg";
+export { default as chatIcon } from './chat.svg';
+export { default as closeIcon } from './close.svg';
+export { default as fileIcon } from './file.svg';
+export { default as hamburger } from './hamburger.svg';
+export { default as optionsIcon } from './options.svg';
+export { default as logoutIcon } from './logout.svg';
+export { default as homeIcon } from './home.svg';
 
-export { default as dp } from "./dp.jpg";
+export { default as dp } from './dp.jpg';
--- src/components/Appbar/Appbar.jsx
+++ src/components/Appbar/Appbar.jsx
@@ -1,72 +1,92 @@
-import React, { useState } from "react";
-import { dp, logoutIcon, closeIcon, searchIcon, hamburger, chatIcon } from "../../assets";
-import { Link } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { logout } from "../../features/userSlice";
-import { fetchPosts, fetchUsers } from "../../API";
-import { toggleSidebar } from "../../features/modalSlice";
-import useFetch from "../../hooks/useFetch";
-import SearchResults from "../SearchResults/SearchResults";
-import "./appbar.css";
+import React, { useState } from 'react';
+import { dp, logoutIcon, closeIcon, searchIcon, hamburger, chatIcon, homeIcon } from '../../assets';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout } from '../../features/userSlice';
+import { fetchPosts, fetchUsers } from '../../API';
+import { toggleSidebar } from '../../features/modalSlice';
+import useFetch from '../../hooks/useFetch';
+import SearchResults from '../SearchResults/SearchResults';
+import './appbar.css';
 
 const Appbar = () => {
-    //global states
-    const {
-        user: { id, token, profileImage },
-        modal: { isSidebarVisible },
-    } = useSelector(state => state);
+	//global states
+	const {
+		user: { id, token, profileImage },
+		modal: { isSidebarVisible },
+	} = useSelector(state => state);
 
-    //local states
-    const [query, setQuery] = useState("");
-    const [searchResult, setSearchResult] = useState({});
+	//local states
+	const [query, setQuery] = useState('');
+	const [searchResult, setSearchResult] = useState({});
 
-    const dispatch = useDispatch();
-    const customFetch = useFetch();
+	const dispatch = useDispatch();
+	const customFetch = useFetch();
 
-    const logoutHandler = () => {
-        dispatch(logout());
-    };
+	const logoutHandler = () => {
+		dispatch(logout());
+	};
 
-    const searchHandler = async e => {
-        e.preventDefault();
-        const { posts } = await customFetch(fetchPosts, token, null, query);
-        const { user } = await customFetch(fetchUsers, token, query);
-        setSearchResult({ posts, user });
-    };
+	const searchHandler = async e => {
+		e.preventDefault();
+		const { posts } = await customFetch(fetchPosts, token, null, query);
+		const { user } = await customFetch(fetchUsers, token, query);
+		setSearchResult({ posts, user });
+	};
 
-    const reset = () => {
-        setQuery("");
-        setSearchResult({});
-    };
+	const reset = () => {
+		setQuery('');
+		setSearchResult({});
+	};
 
-    return (
-        <header className="appbar">
-            <div className="hamburger" onClick={() => dispatch(toggleSidebar(!isSidebarVisible))}>
-                <img src={isSidebarVisible ? closeIcon : hamburger} alt="hamburger" />
-            </div>
-            <form onSubmit={searchHandler} className="searchform">
-                <button type="submit" aria-label="search">
-                    <img src={searchIcon} alt="search" />
-                </button>
-                <input type="text" placeholder="Tap to search..." value={query} onChange={e => setQuery(e.target.value)} />
-                <button onClick={reset} type="button" aria-label="clear search">
-                    <img src={closeIcon} alt="close" className="close" />
-                </button>
-                {(searchResult.posts || searchResult.user) && <SearchResults searchResult={searchResult} reset={reset} />}
-            </form>
-            <nav className="appbar__profile">
-                <Link to={`/user/${id}`}>
-                    <img src={profileImage || dp} alt="profileImage" className="appbar__profile__dp" title="profile" />
-                </Link>
-                <Link to="/chat">
-                    <img src={chatIcon} alt="chat" className="chat" />
-                </Link>
-                <button onClick={logoutHandler} aria-label="logout">
-                    <img src={logoutIcon} alt="logoutIcon" className="appbar__profile__logout" title="logout" />
-                </button>
-            </nav>
-        </header>
-    );
+	return (
+		<header className='appbar'>
+			<div className='hamburger' onClick={() => dispatch(toggleSidebar(!isSidebarVisible))}>
+				<img src={isSidebarVisible ? closeIcon : hamburger} alt='hamburger' />
+			</div>
+			<Link to='/'>
+				<img src={homeIcon} alt='home' className='home-icon' />
+			</Link>
+			<form onSubmit={searchHandler} className='searchform'>
+				<button type='submit' aria-label='search'>
+					<img src={searchIcon} alt='search' />
+				</button>
+				<input
+					type='text'
+					placeholder='Tap to search...'
+					value={query}
+					onChange={e => setQuery(e.target.value)}
+				/>
+				<button onClick={reset} type='button' aria-label='clear search'>
+					<img src={closeIcon} alt='close' className='close' />
+				</button>
+				{(searchResult.posts || searchResult.user) && (
+					<SearchResults searchResult={searchResult} reset={reset} />
+				)}
+			</form>
+			<nav className='appbar__profile'>
+				<Link to={`/user/${id}`}>
+					<img
+						src={profileImage || dp}
+						alt='profileImage'
+						className='appbar__profile__dp'
+						title='profile'
+					/>
+				</Link>
+				<Link to='/chat'>
+					<img src={chatIcon} alt='chat' className='chat' />
+				</Link>
+				<button onClick={logoutHandler} aria-label='logout'>
+					<img
+						src={logoutIcon}
+						alt='logoutIcon'
+						className='appbar__profile__logout'
+						title='logout'
+					/>
+				</button>
+			</nav>
+		</header>
+	);
 };
 
 export default Appbar;
