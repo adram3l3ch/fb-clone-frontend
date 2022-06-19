5e4c9b0da2f17cc67de0b8c16b7b20e334cf9db9
adarsh <adramelech.psd@gmail.com>
2022-06-19 10:05:05 +0530

    change url

--- src/serverUri.js
+++ src/serverUri.js
@@ -1,3 +1,3 @@
-export default process.env.REACT_APP_ENV === 'DEVELOPMENT'
-	? 'http://localhost:5000'
-	: 'https://adramelech-fb-clone.herokuapp.com';
+export default process.env.REACT_APP_ENV === "DEVELOPMENT"
+	? "http://localhost:5000"
+	: "https://adram3l3ch-fb-clone.herokuapp.com";
