58388bf48d9c3f6fdf9e3a7ca69d45be5cff095e
adarsh <adramelech.psd@gmail.com>
2022-06-25 20:20:54 +0530

    update 404 image

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { useNavigate } from "react-router";
-import notfound from "../../assets/not-found.svg";
+import notfound from "../../assets/not-found.png";
 import "./notfound.css";
 
 const NotFound = () => {
-   const navigate = useNavigate();
-   return (
-      <div className="notfound">
-         <img src={notfound} alt="not-found" />
-         <h1>Not Found</h1>
-         <button onClick={() => navigate("/")}>Go Home</button>
-      </div>
-   );
+	const navigate = useNavigate();
+	return (
+		<div className="notfound">
+			<img src={notfound} alt="not-found" />
+			<h1>Not Found</h1>
+			<button onClick={() => navigate("/")}>Go Home</button>
+		</div>
+	);
 };
 
 export default NotFound;
