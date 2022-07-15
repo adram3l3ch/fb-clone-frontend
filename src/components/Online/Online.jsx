76c04e96d8559e9beead3a404f9149c7b8ffb633
Adramelech <adramelech.psd@gmail.com>
2022-07-15 09:57:19 +0530

    add online users to aside

--- src/components/Online/Online.jsx
+++ src/components/Online/Online.jsx
@@ -12,29 +12,38 @@ const Online = () => {
 
 	const dispatch = useDispatch();
 
-	const isOnline = user => {
-		return usersOnline.some(u => u.id === user._id);
+	const allUsers = () => {
+		return users.map(user => (
+			<Link to={`/user/${user._id}`} key={user._id} onClick={() => dispatch(toggleSidebar(false))}>
+				<div className="user">
+					<div>
+						<img src={user.profileImage || dp} alt={user.name + " image"} className="roundimage" />
+					</div>
+					<h3>{user.name}</h3>
+				</div>
+			</Link>
+		));
+	};
+
+	const onlineUsers = () => {
+		const _usersOnline = users.filter(user => usersOnline.some(u => u.id === user._id));
+		return _usersOnline.map(user => (
+			<Link to={`/user/${user._id}`} key={user._id} onClick={() => dispatch(toggleSidebar(false))}>
+				<div className="user" title={user.name}>
+					<div className="green">
+						<img src={user.profileImage || dp} alt={user.name + " image"} className="roundimage" />
+					</div>
+				</div>
+			</Link>
+		));
 	};
 
 	return (
 		<section className="online">
-			<h2>Users</h2>
-			{users.map(user => (
-				<Link
-					to={`/user/${user._id}`}
-					key={user._id}
-					onClick={() => {
-						dispatch(toggleSidebar(false));
-					}}
-				>
-					<div className="user">
-						<div className={isOnline(user) ? "green" : ""}>
-							<img src={user.profileImage || dp} alt={user.name + " image"} className="roundimage" />
-						</div>
-						<h3>{user.name}</h3>
-					</div>
-				</Link>
-			))}
+			<h2>Users Online - {usersOnline.length}</h2>
+			<div className="online-users">{onlineUsers()}</div>
+			<h2>All Users - {users.length}</h2>
+			{allUsers()}
 		</section>
 	);
 };
