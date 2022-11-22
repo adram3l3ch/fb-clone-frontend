8af359bc72f04ea9844bce5c49677681b57c940c
Adramelech <adramelech.psd@gmail.com>
2022-11-22 21:42:14 +0530

    none

--- src/pages/Singlepost/SinglePost.jsx
+++ src/pages/Singlepost/SinglePost.jsx
@@ -21,7 +21,8 @@ const SinglePost = () => {
 			.split("&")
 			.map(ele => ele.split("="))
 	);
-	const { token } = JSON.parse(Cookies.get("user"));
+	const user = Cookies.get("user");
+	const { token } = JSON.parse(user === "undefined" ? {} : user);
 	const { singlePost } = useSelector(state => state.post);
 	const [isLoading, setIsLoading] = useState(true);
 
