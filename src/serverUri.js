df24d92765e279ff079abca38de2fec0e18eed39
Adramelech <adramelech.psd@gmail.com>
2022-12-03 14:15:12 +0530

    change api uri

--- src/serverUri.js
+++ src/serverUri.js
@@ -1,3 +1,1 @@
-export default process.env.NODE_ENV === "development"
-	? "http://localhost:5000"
-	: "https://adram3l3ch-fb-clone.herokuapp.com";
+export default process.env.NODE_ENV === "development" ? "http://localhost:5000" : "amie.up.railway.app";
